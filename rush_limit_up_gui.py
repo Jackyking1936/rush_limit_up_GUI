83f4eeac55650fabb3183b12aa752a01a4b9c124
joukey <j75772763@gmail.com>
2024-06-26 00:22:18 +0800

    layout almost done

--- rush_limit_up_gui.py
+++ rush_limit_up_gui.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from fubon_neo.sdk import FubonSDK, Order
 from fubon_neo.constant import TimeInForce, OrderType, PriceType, MarketType, BSAction
 
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QVBoxLayout, QHBoxLayout, QMessageBox, QTableWidget, QTableWidgetItem, QHeaderView, QPlainTextEdit, QFileDialog
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QGridLayout, QVBoxLayout, QHBoxLayout, QMessageBox, QTableWidget, QTableWidgetItem, QHeaderView, QPlainTextEdit, QFileDialog, QSizePolicy
 from PySide6.QtGui import QTextCursor, QIcon
 from PySide6.QtCore import Qt, Signal, QObject, QMutex
 from threading import Timer
@@ -136,55 +136,107 @@ class MainApp(QWidget):
     def __init__(self):
         super().__init__()
 
+        ### Layout 設定
         my_icon = QIcon()
         my_icon.addFile('fast_icon.png')
 
         self.setWindowIcon(my_icon)
         self.setWindowTitle("Python搶漲停程式教學範例")
-        self.resize(1200, 600)
-        
-        self.mutex = QMutex()
+        self.resize(1000, 600)
         
         # 製作上下排列layout上為庫存表，下為log資訊
         layout = QVBoxLayout()
         # 庫存表表頭
-        self.table_header = ['股票名稱', '股票代號', '類別', '庫存股數', '庫存均價', '現價', '停損', '停利', '損益試算', '獲利率%']
+        self.table_header = ['股票名稱', '股票代號', '成交', '買進', '賣出', '漲幅(%)', '委託數量', '成交數量']
         
         self.tablewidget = QTableWidget(0, len(self.table_header))
         self.tablewidget.setHorizontalHeaderLabels([f'{item}' for item in self.table_header])
+
+        # 整個設定區layout
+        layout_condition = QGridLayout()
+
+        # 監控區layout
+        label_monitor = QLabel('監控設定')
+        layout_condition.addWidget(label_monitor, 0, 0)
+        label_up_range = QLabel('漲幅(%)')
+        layout_condition.addWidget(label_up_range, 1, 0)
+        self.lineEdit_up_range = QLineEdit()
+        self.lineEdit_up_range.setText('7')
+        layout_condition.addWidget(self.lineEdit_up_range, 1, 1)
+        label_up_range_post = QLabel('以上')
+        layout_condition.addWidget(label_up_range_post, 1, 2)
+        label_freq = QLabel('定時每')
+        layout_condition.addWidget(label_freq, 2, 0)
+        self.lineEdit_freq = QLineEdit()
+        self.lineEdit_freq.setText('5')
+        layout_condition.addWidget(self.lineEdit_freq, 2, 1)
+        label_freq_post = QLabel('秒更新')
+        layout_condition.addWidget(label_freq_post, 2, 2)
+
+        # 交易區layout
+        label_trade = QLabel('交易設定')
+        layout_condition.addWidget(label_trade, 0, 3)
+        label_trade_budget = QLabel('每檔額度')
+        layout_condition.addWidget(label_trade_budget, 1, 3)
+        self.lineEdit_trade_budget = QLineEdit()
+        self.lineEdit_trade_budget.setText('10')
+        layout_condition.addWidget(self.lineEdit_trade_budget, 1, 4)
+        label_trade_budget_post = QLabel('萬元')
+        layout_condition.addWidget(label_trade_budget_post, 1, 5)
+
+        # 啟動按鈕
+        self.button_start = QPushButton('開始洗價')
+        self.button_start.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        self.button_start.setStyleSheet("QPushButton { font-size: 24px; font-weight: bold; }")
+        layout_condition.addWidget(self.button_start, 0, 6, 3, 1)
         
+        # 模擬區Layout設定
         self.fake_buy = QPushButton('fake buy filled')
         self.fake_sell = QPushButton('fake sell filled')
         self.fake_websocket = QPushButton('fake websocket')
         
-        layoutH = QHBoxLayout()
-        layoutH.addWidget(self.fake_buy)
-        layoutH.addWidget(self.fake_sell)
-        layoutH.addWidget(self.fake_websocket)
+        layout_sim = QGridLayout()
+        label_sim = QLabel('測試用按鈕')
+        label_sim.setStyleSheet("QLabel { font-size: 24px; font-weight: bold; }")
+        label_sim.setAlignment(Qt.AlignCenter)
+        layout_sim.addWidget(label_sim, 0, 1)
+        layout_sim.addWidget(self.fake_buy, 1, 0)
+        layout_sim.addWidget(self.fake_sell, 1, 1)
+        layout_sim.addWidget(self.fake_websocket, 1, 2)
         
+        # Log區Layout設定
         self.log_text = QPlainTextEdit()
         self.log_text.setReadOnly(True)
 
         layout.addWidget(self.tablewidget)
-        layout.addLayout(layoutH)
+        layout.addLayout(layout_condition)
+        layout.addLayout(layout_sim)
         layout.addWidget(self.log_text)
         self.setLayout(layout)
 
+        ### 建立連線開始跑主要城市
         self.print_log("login success, 現在使用帳號: {}".format(active_account.account))
         self.print_log("建立行情連線...")
         sdk.init_realtime() # 建立行情連線
         self.print_log("行情連線建立OK")
         self.reststock = sdk.marketdata.rest_client.stock
         
-        # 初始化庫存表資訊
-        self.stop_loss_dict = {}
-        self.take_profit_dict = {}
-        self.inventories = {}
-        self.unrealized_pnl = {}
-        self.row_idx_map = {}
+        # 初始化參數資訊
         self.epsilon = 0.0000001
+        self.row_idx_map = {}
         self.col_idx_map = dict(zip(self.table_header, range(len(self.table_header))))
-    
+
+        # slot function connect
+        self.button_start.clicked.connect(self.on_button_start_clicked)
+
+    def on_button_start_clicked(self):
+        self.print_log("開始執行")
+        self.lineEdit_up_range.setReadOnly(True)
+        self.lineEdit_freq.setReadOnly(True)
+        self.lineEdit_trade_budget.setReadOnly(True)
+        self.button_start.setVisible(False)
+        
+
     # 更新最新log到QPlainTextEdit的slot function
     def print_log(self, log_info):
         self.log_text.appendPlainText(log_info)
